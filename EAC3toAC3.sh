bc59f919c14ebf618edafcef743b3ff3cf533387
koolwe <mrshahg786@gmail.com>
2020-02-14 00:46:50 +0500

    Update EAC3toAC3.sh

--- EAC3toAC3.sh
+++ EAC3toAC3.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-for f in *.mkv; do ffmpeg -y -i "$f" -map 0 -c:v copy -c:s copy -c:a ac3 -b:a 640k "${f%.*}-ac3.mkv"; done
+for f in *.mkv; do ffmpeg -y -i "$f" -map 0 -crf 0 -c:v copy -c:s copy -c:a ac3 -b:a 640k "${f%.*}-ac3.mkv"; done
