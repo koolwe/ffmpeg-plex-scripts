03dc9ca0d21c3c9d5b9dc5239a1a672b32fd170e
koolwe <mrshahg786@gmail.com>
2020-02-14 00:46:28 +0500

    Update AC3toAAC.sh

--- AC3toAAC.sh
+++ AC3toAAC.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-for f in *.mkv; do ffmpeg -y -i "$f" -map 0:a:0? -strict -2 -c:v copy -c:s copy -c:a aac -b:a 400k "${f%.*}-aac.mkv"; done
+for f in *.mkv; do ffmpeg -y -i "$f" -map 0:a:0? -strict -2 -crf 0 -c:v copy -c:s copy -c:a aac -b:a 400k "${f%.*}-aac.mkv"; done
